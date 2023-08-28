d5873a29052aa587513a42bf11fcbfc9e353a8d0
김태호 <taehokim@hicare.net>
2023-08-28 17:26:00 +0900

    feat image search network api applied on HomeScreen, HomeViewModel

--- app/src/main/java/com/jaden/mycats/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/jaden/mycats/ui/screens/HomeScreen.kt
@@ -1,30 +1,81 @@
 package com.jaden.mycats.ui.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.jaden.mycats.ui.models.HomeImageModel
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        items(homeViewModel.homeImageModels) { homeImageModel ->
-            Text(text = homeImageModel.uuid.toString())
+
+    val imageModels by homeViewModel.getImagesFlow().collectAsState(initial = emptyList())
+
+    LazyColumn {
+        items(imageModels) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .padding(20.dp)
+            ) {
+                AsyncImage(
+                    model = it.url,
+                    contentScale = ContentScale.FillWidth,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = { /* TODO : do favorite the picture */ },
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .align(Alignment.BottomEnd)
+                ) {
+                    Icon(Icons.Filled.Favorite, null)
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    val imageModels = listOf(
+        HomeImageModel("url 1"),
+        HomeImageModel("url 2"),
+        HomeImageModel("url 3"),
+        HomeImageModel("url 4"),
+        HomeImageModel("url 5"),
+        HomeImageModel("url 6"),
+    )
+
+    LazyColumn {
+       items(imageModels) {
+           AsyncImage(
+               model = it.url,
+               contentDescription = null,
+           )
+       }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun Preview_HomeScreen() {
-    val viewModel: HomeViewModel = hiltViewModel()
-    HomeScreen(homeViewModel = viewModel)
+    HomeScreen()
 }
--- app/src/main/java/com/jaden/mycats/ui/screens/HomeViewModel.kt
+++ app/src/main/java/com/jaden/mycats/ui/screens/HomeViewModel.kt
@@ -1,17 +1,28 @@
 package com.jaden.mycats.ui.screens
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.jaden.mycats.ui.models.HomeImageModel
+import com.jaden.core_network.ImageSearchService
+import com.jaden.core_network.response.ImageInformationList
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val imageSearchService: ImageSearchService,
+): ViewModel() {
 
-    val homeImageModels = listOf(
-        HomeImageModel(""),
-        HomeImageModel(""),
-        HomeImageModel(""),
-        HomeImageModel(""),
-    )
+    fun getImagesFlow(): Flow<ImageInformationList> = try {
+        imageSearchService.getImages({
+            Log.d("Home View Model", "On Start flow")
+        }, {
+            Log.d("Home View Model", "On Complete flow")
+        })
+    } catch (exception: Exception) {
+        Log.d("", "Error :: ${exception.message}")
+        emptyFlow()
+    }
+
 }
